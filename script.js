a0fdcdf17fc04055a7652684b990e80fe8b93967
Sjoerd Boerrigter <sjoerd@trendwerk.nl>
2015-06-07 18:17:25 +0200

    Added more sounds

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 	/**
 	 * Play sounds on button click
 	 */
-	$('#badum-tss button').click(function(){
-		var audio = $('#badum-tss').find('audio').get(0);
+	$('#rimshot button').click(function(){
+		var audio = $('#rimshot').find('audio').get(0);
 		if (audio.paused) {
 			audio.play();
 		}else{
@@ -19,6 +19,14 @@ $(document).ready(function(){
 			audio.currentTime = 0;
 		}
 	});
+	$('#fart button').click(function(){
+		var audio = $('#fart').find('audio').get(0);
+		if (audio.paused) {
+			audio.play();
+		}else{
+			audio.currentTime = 0;
+		}
+	});
 	$('#dancehall-horn button').click(function(){
 		var audio = $('#dancehall-horn').find('audio').get(0);
 		if (audio.paused) {
@@ -27,8 +35,32 @@ $(document).ready(function(){
 			audio.currentTime = 0;
 		}
 	});
+	$('#gayyy button').click(function(){
+		var audio = $('#gayyy').find('audio').get(0);
+		if (audio.paused) {
+			audio.play();
+		}else{
+			audio.currentTime = 0;
+		}
+	});
+	$('#dramatic button').click(function(){
+		var audio = $('#dramatic').find('audio').get(0);
+		if (audio.paused) {
+			audio.play();
+		}else{
+			audio.currentTime = 0;
+		}
+	});
 	$('#best-cry-ever button').click(function(){
 		var audio = $('#best-cry-ever').find('audio').get(0);
+		if (audio.paused) {
+			audio.play();
+		}else{
+			audio.currentTime = 0;
+		}
+	});
+	$('#trololol button').click(function(){
+		var audio = $('#trololol').find('audio').get(0);
 		if (audio.paused) {
 			audio.play();
 		}else{
